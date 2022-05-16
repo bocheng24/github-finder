3d1fe608ae2172e125f5dde7f9c424716a1fd664
bocheng24 <jeffcgp@hotmail.com>
2022-05-16 22:42:03 +0800

    setup notfound page and routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./components/Navbar/Navbar";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
           <Routes>
             <Route path='/' element={ <Home /> } />
             <Route path='about' element={ <About /> } />
+            <Route path='/notfound' element={ <NotFound /> } />
+            <Route path='/*' element={ <NotFound /> } />
           </Routes>
         </BrowserRouter>
     </div>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <section className="flex flex-col items-center justify-center w-full">
+        <div>
+            <h1 className="text-5xl">404 Page Not Found</h1>
+        </div>
+    </section>
+  )
+}
+
+export default NotFound
